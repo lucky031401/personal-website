03b50b6dbbeb5c8bf69682e25c8bb526e624715b
lucky031401 <lucky3748@gmail.com>
2021-04-15 12:53:35 +0800

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
     <h1>Home page</h1>
-    <NuxtLink to="/about">
+    <NuxtLink to="/test0414">
       About (internal link that belongs to the Nuxt App)
     </NuxtLink>
     <a href="https://nuxtjs.org">External Link to another page</a>
   </main>
 </template>
 <script>
+  
 </script>
 
 <style>
---
+++ pages/test0414/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+    <div id="app" class="static" v-bind:class="{ active: isActive }" style="width:200px;height:200px">hi vue</div>
+  </main>
+</template>
+<script>
+  export default{
+      data(){
+        return{
+          isActive:true
+        }
+      }
+  }
+</script>
+
+<style>
+.active{background: skyblue;}
+</style>
